0d7fb84833c46e227f4299b85a5ce914e5656104
Jao <125588400+Gui1herm3@users.noreply.github.com>
2024-05-02 08:48:15 -0300

    Update and rename Node.js.txt to Texto principal.js

---
+++ Node/Texto principal.js
@@ -0,0 +1,20 @@
+  const mysql = require('mysql2');
+
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'phpmyadmin',
+      password: 'aluno',
+      database: 'ATIVIDADE3004'
+    });
+
+    connection.connect();
+
+    const queryString = 'SELECT * FROM CARROS';
+
+    connection.query(queryString, (error, results, fields) => {
+      if (error) throw error;
+      console.log('Resultado da consulta:');
+      console.log(results);
+    });
+
+    connection.end();
