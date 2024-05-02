53d4543dce0a37806a6c8f39c4fe496742edb9bb
Jao <125588400+Gui1herm3@users.noreply.github.com>
2024-05-02 08:56:33 -0300

    Create cor_verde.js

---
+++ Node/cor_verde.js
@@ -0,0 +1,20 @@
+  4.8                                            Node.js                                                           const mysql = require('mysql2');
+
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'phpmyadmin',
+      password: 'aluno',
+      database: 'ATIVIDADE3004'
+    });
+
+    connection.connect();
+
+    const queryString = "SELECT * FROM CARROS WHERE COR = 'VERDE'";
+
+    connection.query(queryString, (error, results, fields) => {
+      if (error) throw error;
+      console.log('Resultado da consulta:');
+      console.log(results);
+    });
+
+    connection.end
